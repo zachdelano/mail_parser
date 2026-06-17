da5b68e19a95099b2e47ea22cf1b3abb40128c37
Zach Delano <z.delano@outlook.com>
2026-06-16 23:48:21 -0600

    fix: refactor check_escaped to advance iterator and handle backslash counts

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,14 +3,15 @@ use crate::models::ParsedEmail;
 use scanner::Scanner;
 use std::collections::VecDeque;
 
-fn check_escaped(s: &Scanner) -> bool {
-    let mut scanner = s.clone();
+fn check_escaped(scanner: &mut Scanner) -> (bool, i32) {
+    // let mut scanner = s.clone();
     let mut count = 0;
     while let Some((_, '\\')) = scanner.peek_char() {
         scanner.next_char();
         count += 1;
     }
-    count % 2 != 0
+    // return whether escaped, then the number of escaped `\` characters encountered 
+    (count % 2 != 0, count / 2)
 }
 
 /// The main entry point for the parsing logic.
@@ -32,8 +33,10 @@ pub fn parse_address(input: &str) -> Result<ParsedEmail, String> {
         // `comment_level > 0` means that a comment block has already been encountered
         //    and not yet closed.
         match ch {
+            // TODO: what about backslashes that aren't followed by a parenthesis?
+            //    do we need to handle that as well? we'll handle it later.
             ')' => {
-                let is_escaped = check_escaped(&scanner);
+                let (is_escaped, backslash_count) = check_escaped(&mut scanner);
                 if !is_escaped {
                     comment_level += 1;
                     if comments_raw.len() < (comment_idx + 1) {
@@ -43,9 +46,14 @@ pub fn parse_address(input: &str) -> Result<ParsedEmail, String> {
                 if comment_level > 1 || (comment_level == 1 && is_escaped) {
                     comments_raw[comment_idx].push_front(ch);
                 }
+                if comment_level > 0 {
+                    for _ in 0..backslash_count {
+                        comments_raw[comment_idx].push_front('\\')
+                    }
+                }
             }
             '(' => {
-                let is_escaped = check_escaped(&scanner);
+                let (is_escaped, backslash_count) = check_escaped(&mut scanner);
                 if comment_level > 0 && !is_escaped {
                     comment_level -= 1;
                     if comment_level == 0 {
@@ -54,6 +62,9 @@ pub fn parse_address(input: &str) -> Result<ParsedEmail, String> {
                 }
                 if comment_level > 0 {
                     comments_raw[comment_idx].push_front(ch);
+                    for _ in 0..backslash_count {
+                        comments_raw[comment_idx].push_front('\\')
+                    }
                 }
             }
             _ => {
